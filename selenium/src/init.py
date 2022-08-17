b51761347762e1274a996cee60c068af0ce05e63
nks <xnks@gsitm.com>
2022-08-18 03:39:47 +0900

    업데이트

--- selenium/src/init.py
+++ selenium/src/init.py
@@ -20,28 +20,29 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 
-def getChromeOptions():
-    return 'a'
+# Chrome 브라우저 옵션을 설정
+def getChromeOptions(profile):
+    
+    chrome_options = Options()
 
-chrome_options = Options()
+    user_agent = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.83 Safari/537.36 '
+    chrome_options.add_argument('user-agent=' + user_agent)
+    #chrome_options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
+    chrome_options.add_argument('--window-size= x, y') #실행되는 브라우저 크기를 지정할 수 있습니다.
+    chrome_options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
+    #chrome_options.add_argument('--start-fullscreen') #브라우저가 풀스크린 모드(F11)로 실행됩니다.
+    #chrome_options.add_argument('--blink-settings=imagesEnabled=false') #브라우저에서 이미지 로딩을 하지 않습니다.
+    chrome_options.add_argument('--mute-audio') #브라우저에 음소거 옵션을 적용합니다.
+    chrome_options.add_argument('--user-data-dir=C:/Users/mypc/AppData/Local/Google/Chrome/User Data') #사용자 환경설정 경로
+    #chrome_options.add_argument('--profile-directory=profile 2') #사용자 환경설정 경로
+    #chrome_options.add_argument('--profile-directory=Default') #사용자 환경설정 경로
+    chrome_options.add_argument('--profile-directory='+profile)
+    #chrome_options.add_argument('incognito') #시크릿 모드의 브라우저가 실행됩니다.
+    
+    return chrome_options
 
-#지정한 user-agent로 설정합니다.
-#user_agent = "Mozilla/5.0 (Linux; Android 9; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.83 Safari/537.36"
-user_agent = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36 '
-chrome_options.add_argument('user-agent=' + user_agent)
 
-#chrome_options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
-chrome_options.add_argument('--window-size= x, y') #실행되는 브라우저 크기를 지정할 수 있습니다.
-chrome_options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
-#chrome_options.add_argument('--start-fullscreen') #브라우저가 풀스크린 모드(F11)로 실행됩니다.
-#chrome_options.add_argument('--blink-settings=imagesEnabled=false') #브라우저에서 이미지 로딩을 하지 않습니다.
-chrome_options.add_argument('--mute-audio') #브라우저에 음소거 옵션을 적용합니다.
-chrome_options.add_argument('--user-data-dir=C:/Users/mypc/AppData/Local/Google/Chrome/User Data') #사용자 환경설정 경로
-#chrome_options.add_argument('--profile-directory=profile 2') #사용자 환경설정 경로
-chrome_options.add_argument('--profile-directory=Default') #사용자 환경설정 경로
-#chrome_options.add_argument('incognito') #시크릿 모드의 브라우저가 실행됩니다.
-
-driver = webdriver.Chrome(r'C:\Dev\git\python-study\selenium\files\chromedriver.exe', options=chrome_options)
+driver = webdriver.Chrome(r'C:\Dev\git\python-study\selenium\files\chromedriver.exe', options=getChromeOptions('Default'))
 #driver = webdriver.Remote('http://localhost:4444/wd/hub', chrome_options.to_capabilities())
 url = 'https://mr-seo.co.kr/auth/login'
 driver.get(url)
